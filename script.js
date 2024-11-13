60f284fe9c21d861ae45d6652c2a9dae371cd881
Llntrvr <92546061+Llntrvr@users.noreply.github.com>
2024-11-12 17:50:00 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Going to Camp Auto Click
 // @namespace    http://goingtocamp.com
-// @version      2024-11-15
+// @version      20241112549pm
 // @description  Try to auto reserve campsites
 // @author       Trevor Dilley
 // @match        https://washington.goingtocamp.com/create-booking/*
@@ -27,6 +27,8 @@
         $('button#addToStay').click();
     }
 
+
+
     function clock()
     {
         setInterval(function() {
@@ -45,14 +47,14 @@
               clickEvent();
             }
 
-            $('#clock').html(
+            $('span#clock').html(
                 'Current Time: <strong>'+hours + ":" + minutes + ":" + seconds+'</strong> <i>Pacific Standard Time</i>'
             );
         }, 500);
     }
 
     function loadUI(){
-        $('body').prepend('<div style="padding:10px;border-bottom:5px solid #ff0000;"><strong>Please make sure to pre select the correct camsite now.</strong><p>Make sure to be on the computer by 7:10 AM <i>Pacific Standard Time</i> to complete the reservation!</p><div id="clock"></div> <div>Will Fire: <strong>6:59:00 AM</strong> - <strong>7:02:59 AM</strong> <i>Pacific Standard Time</i></div> </div>');
+        $('body').prepend('<div style="padding:10px;border-bottom:5px solid #ff0000;"><div id="pleaseselect" style="font-size:30px;font-weight:bold;color:#ff0000;text-align:center;"><strong>Please make sure to pre select the correct camsite now!</strong></div><div id="notice">Make sure to be on the computer by 7:10 AM <i>Pacific Standard Time</i> to complete the reservation! <br> <span id="clock"></span> <br> Will Fire: <strong>6:59:00 AM</strong> - <strong>7:02:59 AM</strong> <i>Pacific Standard Time</i></div> </div>');
     }
 
 })();
