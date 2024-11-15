c2a5ecbb2dfdba781e1e969fe8881bfdb2eb9d76
Llntrvr <92546061+Llntrvr@users.noreply.github.com>
2024-11-15 03:41:01 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Going to Camp Auto Click
 // @namespace    https://washington.goingtocamp.com
-// @version      2024112144
+// @version      20241115340
 // @description  Try to auto reserve campsites
 // @author       Trevor Dilley
 // @match        https://washington.goingtocamp.com/create-booking/*
@@ -88,7 +88,7 @@
                 '<strong>'+hours + ":" + minutes + ":" + seconds+'</strong> <i>'+timezone+'</i>'
             );
 
-            if(match >= '065959' && match <= '070001')
+            if(match == '070000')
             {
                 clickEvent();
             }
@@ -101,7 +101,7 @@
     }
 
     function loadUI(){
-        $('body').prepend('<div id="topbanner" style="padding:10px;border-bottom:5px solid #ff0000;"><div id="pleaseselect" style="font-size:30px;font-weight:bold;text-align:center;"></div><div id="notice"><ol><li>Make sure you are logged into your <a href="/login" target="_blank">account</a>.</li><li id="visiblecheck">Make sure this tab / window is visible at all times or the timmer will not work.</li><li>Make sure to disable any windows/mac computer sleep mode / lock screen.</li><li>Make sure to be on the computer by 7:05 AM <i>'+timezone+'</i> to complete the <a href="/cart" target="_blank">reservation</a>.</li></ol> <ul><li> System Time: <span id="clock">00:00:00 AM</span>.</li> <li>Will Fire Between: <strong>6:59:59 AM</strong> - <strong>7:00:01 AM</strong> <i>'+timezone+'</i>.</li></ul></div> </div>');
+        $('body').prepend('<div id="topbanner" style="padding:10px;border-bottom:5px solid #ff0000;"><div id="pleaseselect" style="font-size:30px;font-weight:bold;text-align:center;"></div><div id="notice"><ol><li>Make sure you are logged into your <a href="/login" target="_blank">account</a>.</li><li id="visiblecheck">Make sure this tab / window is visible at all times or the timmer will not work.</li><li>Make sure to disable any windows/mac computer sleep mode / lock screen.</li><li>Make sure to be on the computer by 7:05 AM <i>'+timezone+'</i> to complete the <a href="/cart" target="_blank">reservation</a>.</li></ol> <ul><li> System Time: <span id="clock">00:00:00 AM</span>.</li> <li>Will Fire At: <strong>7:00:00 AM</strong> <i>'+timezone+'</i>.</li></ul></div> </div>');
     }
 
 })();
